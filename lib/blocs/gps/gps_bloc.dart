3d1b8829e1bc0c8f8564492a5ab734746f1c4a93
Bryan Parada <bryan.parada.cea@gmail.com>
2023-03-06 21:06:23 -0300

    FA188 - Geolocator - android

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -12,5 +13,36 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         isGpsPermissionGranted: event.isGpsPermissionGranted
       )) 
     );
+
+  _init();
+
   }
+
+  Future<void> _init() async {
+
+    final isEnabled = await _checkGpsStatus();
+    print('isEnabled: $isEnabled');
+    
+  }
+
+  Future<bool> _checkGpsStatus() async{
+
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
+      print('service status $isEnabled');
+    });
+
+    return isEnable;
+
+  }
+
+  @override
+  Future<void> close() {
+    //TODO: limpiar el servicestatus stream
+    return super.close();
+  }
+
+
 }
