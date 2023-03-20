7b696844cd0778c2f887eab39e29807f597b1262
Bryan Parada <bryan.parada.cea@gmail.com>
2023-03-20 19:02:54 -0300

    FA270 - Text in CustomPainter

--- lib/markers/start_marker.dart
+++ lib/markers/start_marker.dart
@@ -42,6 +42,41 @@ class StartMarkerPainter extends CustomPainter{
     // Caja negra
     const blackBox = Rect.fromLTWH(40, 20, 70, 80);
     canvas.drawRect(blackBox, blackPaint);
+
+    //Textos
+    //Minutos
+    final textSpan = TextSpan(
+      style: const TextStyle( color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+      text: '55'
+    );
+
+    final minutesPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr, //left to right
+      textAlign: TextAlign.center
+    )..layout(
+      minWidth: 70,
+      maxWidth: 70
+    );
+
+    minutesPainter.paint( canvas, const Offset( 40, 35 ));
+
+    //palabra Min
+    final minutesText = TextSpan(
+      style: const TextStyle( color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),
+      text: 'Min'
+    );
+
+    final minutesMinPainter = TextPainter(
+      text: minutesText,
+      textDirection: TextDirection.ltr, //left to right
+      textAlign: TextAlign.center
+    )..layout(
+      minWidth: 70,
+      maxWidth: 70
+    );
+
+    minutesMinPainter.paint( canvas, const Offset( 40, 68 ));
     
   }
 
